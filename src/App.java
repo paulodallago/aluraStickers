a5ce64e91ec4e8ec1e1a4da438842a390d3eb9d7
paulodallago <paulohdallago@gmail.com>
2023-04-06 01:14:05 -0300

    add commentary and readme

--- src/App.java
+++ src/App.java
@@ -6,14 +6,17 @@ import java.io.InputStream;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        //Api pública Nasa
         // String url = "https://api.nasa.gov/planetary/apod?api_key=RzqhhvuF5WfclH3nHbnMHy9XCAADvZrQxVPoLWZH&start_date=2022-06-12&end_date=2022-06-14";
         // contentExtractor extrator = new contentAPOD();
 
-        String url = "http://localhost:8080/linguagens";
-        contentExtractor extrator = new contentIMDB();
-
+        //Api pública IMDB
         // String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
         // contentExtractor extrator = new contentIMDB();
+
+        //Minha Api (linguagens de programação)
+        String url = "http://localhost:8080/linguagens";
+        contentExtractor extrator = new contentIMDB();
 
         var http = new ClientHttp();
         String json = http.buscaDados(url);
